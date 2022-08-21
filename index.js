5406f8cae2769a5a42705f9ec8e4028d9bf1289e
victor-di-bar <victor_damm_8@hotmail.com>
2022-08-21 12:44:58 +0200

    Añadida lógica trios, suma de puntuación y suma de 1s y 5s revisada

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const randomGenerator = () => {
-  const resultado1 = random(1, 6);
-  const resultado2 = random(1, 6);
-  const resultado3 = random(1, 6);
-  const resultado4 = random(1, 6);
-  const resultado5 = random(1, 6);
-  const resultado6 = random(1, 6);
+//   const resultado1 = random(1, 6);
+//   const resultado2 = random(1, 6);
+//   const resultado3 = random(1, 6);
+//   const resultado4 = random(1, 6);
+//   const resultado5 = random(1, 6);
+//   const resultado6 = random(1, 6);
 
-  let dados = [
-    resultado1,
-    resultado2,
-    resultado3,
-    resultado4,
-    resultado5,
-    resultado6,
-  ];
+  let dados = [1,1,1,5,5,5];
+
+  let arrayUnos = [];
+  let arrayCincos = [];
+
+  for (dado of dados) {
+    if (dado === 1) {
+        arrayUnos.push(dado);
+    } else if (dado === 5) {
+        arrayCincos.push(dado);
+    }
+  }
+
+  resultadoAnalisisUnosCincos.innerHTML = `Tienes ${arrayUnos.length} uno(s) y ${arrayCincos.length} cinco(s)`;
+
+  let resultadoUnosCincos = ((100 * arrayUnos.length) + (50 * arrayCincos.length));
 
   let triplicados = [];
   const dadosOrdenados = dados.sort();
@@ -28,20 +36,30 @@ const randomGenerator = () => {
   }
 
   resultado.innerHTML = `Tirada ${dados}`;
-  resultadoAnalisis.innerHTML = `Tríos ${triplicados}`;
+  resultadoAnalisisTrios.innerHTML = `Trío(s) ${triplicados}`;
 
-  let calculoPuntuacion = 0;
+  let calculoPuntuacionTrios = 0;
 
   for (x of triplicados) {
-    calculoPuntuacion = x * 100;
+    if (x === 1) {
+        calculoPuntuacionTrios += 1000;
+        resultadoUnosCincos = (50 * arrayCincos.length);
+    } else if (x === 5) {
+        resultadoUnosCincos = (100 * arrayUnos.length);
+        calculoPuntuacionTrios += x * 100;
+    } else {
+        calculoPuntuacionTrios += x * 100;
+    }
   }
 
-  puntuacion.innerHTML = `Tu puntuación actual es ${calculoPuntuacion}`;
+  let puntuacionFinal = calculoPuntuacionTrios + resultadoUnosCincos;
+
+  puntuacion.innerHTML = `Tu puntuación actual es ${puntuacionFinal}`;
 };
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function random(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
 const button = document.getElementById("lanzar-button");
 button.addEventListener("click", randomGenerator);
